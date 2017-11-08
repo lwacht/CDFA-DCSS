8d8171b2213b8122a5d1a4812f6472884988c0b9
John Towell <john.towell@gmail.com>
2017-11-08 10:23:45 -0800

    add file date to delinquencyFileDate array (#4)

--- src/import/delinquency-dynamodb-write.js
+++ src/import/delinquency-dynamodb-write.js
@@ -4,6 +4,7 @@
 const {Writable} = require('stream');
 const AWS = require('aws-sdk');
 const TABLE_NAME = process.env.TABLE_NAME;
+
 if (TABLE_NAME === 'dcss-local') {
     AWS.config.update({
         endpoint: "http://localhost:8000",
@@ -13,24 +14,87 @@ if (TABLE_NAME === 'dcss-local') {
 const dynamodb = new AWS.DynamoDB();
 
 module.exports = {
-    writer: new Writable({
-        objectMode: true,
-        write(chunk, encoding, callback) {
-            console.log(chunk);
-            console.log(TABLE_NAME);
-            let params = {
-                TableName: TABLE_NAME,
-                Item: chunk
-            };
-            dynamodb.putItem(params, function (err, data) {
-                if (err) {
-                    console.log(err, err.stack);
-                    callback(err);
-                } else {
-                    console.log(data);
-                    callback();
-                }
-            });
-        }
-    })
+    writer: function(fileName) {
+        return new Writable({
+            objectMode: true,
+            write(chunk, encoding, callback) {
+                console.log(chunk);
+                let record = {
+                    id: chunk.id,
+                    participant: chunk,
+                    delinquent: {BOOL: true}
+                };
+                delete record.participant.id;
+                addUpdateItem(record)
+                    .then(updateFileDate.bind(null, record, fileName))
+                    .then(function () {
+                        callback();
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                        callback(err);
+                    });
+            }
+        })
+    }
 };
+
+/**
+ * Adds a new record or updates it if it exists already.
+ *
+ * Returns a promise.
+ */
+function addUpdateItem(item) {
+    let id = item.id;
+    let params = {
+        ExpressionAttributeNames: {
+            "#P": "participant",
+            "#D": "delinquent"
+        },
+        ExpressionAttributeValues: {
+            ":p": {
+                M: item.participant
+            },
+            ":d": item.delinquent,
+        },
+        Key: {"id": id},
+        ReturnValues: "ALL_NEW",
+        TableName: TABLE_NAME,
+        UpdateExpression: "SET #D = :d, #P = :p"
+    };
+    return dynamodb.updateItem(params).promise();
+}
+
+/**
+ * Updates the delinquencyFileDate string array with the date of the file passed in.
+ *
+ * Returns a promise
+ */
+function updateFileDate(item, fileName) {
+    let params = {
+        ExpressionAttributeNames: {
+            "#D": "delinquencyFileDate"
+        },
+        ExpressionAttributeValues: {
+            ":d": {
+                SS: [parseDateString(fileName)]
+            }
+        },
+        Key: {id: item.id},
+        ReturnValues: "ALL_NEW",
+        TableName: TABLE_NAME,
+        UpdateExpression: "ADD #D :d"
+    };
+
+    return dynamodb.updateItem(params).promise();
+
+    /**
+     * Converts the file name into a date string
+     *
+     * parseDateString(SLM_Delinquency_20171108.txt) // 20171108
+     */
+    function parseDateString(name) {
+        let underscores = name.split("_");
+        return underscores[underscores.length-1].split(".")[0];
+    }
+}
--- test/import/delinquency-dynamodb-write.test.js
+++ test/import/delinquency-dynamodb-write.test.js
@@ -1,5 +1,6 @@
 process.env.TABLE_NAME = 'dcss-local';
 const fs = require('fs');
+const attr = require('dynamodb-data-types').AttributeValue;
 const transform = require("../../src/import/delinquency-json-transform");
 const write = require("../../src/import/delinquency-dynamodb-write");
 const AWS = require('aws-sdk');
@@ -10,11 +11,13 @@ AWS.config.update({
 });
 const dynamodb = new AWS.DynamoDB();
 
+const fileName = 'SLM_Delinquency_20171108.txt';
+
 test('write to dynamodb', (done) => {
 
     fs.createReadStream("test/import/delinquency-import-test-1.txt")
         .pipe(transform.jsonTransform)
-        .pipe(write.writer)
+        .pipe(write.writer(fileName))
         .on('finish', function () {
             let params = {
                 Key: {
@@ -37,9 +40,13 @@ test('write to dynamodb', (done) => {
         });
 
     let validate = (data)=> {
+        data = attr.unwrap(data);
+        console.log(data);
         //validating hydration of data, full data map is done in transform test
-        expect(data.id.S).toBe("1");
-        expect(data.firstName.S).toBe("JOHN");
+        expect(data.id).toBe("1");
+        expect(data.participant.firstName).toBe("JOHN");
+        expect(data.delinquent).toBe(true);
+        expect(data.delinquencyFileDate).toContain('20171108');
     };
 });
 
